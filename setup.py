4a6e337acf9c2dde6634715e58098494af9ccd56
David R. Bild <david@davidbild.org>
2015-02-24 11:54:52 -0600

    create project skeleton

---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name='stratiform',
+    version='0.0.0',
+    author = "David R. Bild",
+    author_email = "david@davidbild.org",
+    description="Concise creation of AWS CloudFormation templates.",
+    keywords = "cloudformation aws amazon template",
+    url="https://github.com/drbild/stratiform",
+    packages=['stratiform'],
+    classifiers = [
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python'
+    ]
+)
